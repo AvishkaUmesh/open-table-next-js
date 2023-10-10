fbdbdd1aee584452c08bd52b98d6bbb05b0b884c
Avishka Umesh <umesh.avishka@gmail.com>
2023-10-10 17:43:10 +0530

    added search by location functionality

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -1,31 +1,38 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 
 function SearchBar() {
   const router = useRouter();
-  const [location, setLocation] = useState('');
+  const [location, setLocation] = useState<string>('');
 
-  const onSearch = () => {
-    router.push(`/search`);
+  const onSearch = (e: FormEvent) => {
+    e.preventDefault();
+    if (!location) {
+      return;
+    }
+    router.push(`/search?city=${location}`);
+    setLocation('');
   };
 
   return (
     <div className="m-auto flex justify-center py-3 text-left text-lg">
-      <input
-        className="mr-3  w-[450px] rounded p-2"
-        type="text"
-        placeholder="State, city or town"
-        value={location}
-        onChange={(e) => setLocation(e.target.value)}
-      />
-      <button
-        className="rounded bg-red-600 px-9 py-2 text-white"
-        onClick={onSearch}
-      >
-        Let&apos;s go
-      </button>
+      <form onSubmit={onSearch}>
+        <input
+          className="mr-3  w-[450px] rounded p-2"
+          type="text"
+          placeholder="State, city or town"
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+        />
+        <button
+          className="rounded bg-red-600 px-9 py-2 text-white"
+          type="submit"
+        >
+          Let&apos;s go
+        </button>
+      </form>
     </div>
   );
 }
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,3 +1,4 @@
+import { PrismaClient } from '@prisma/client';
 import { Metadata } from 'next';
 import Header from './components/Header';
 import RestaurantCard from './components/RestaurantCard';
@@ -8,14 +9,52 @@ export const metadata: Metadata = {
   description: 'Book a table at your favorite restaurant',
 };
 
-function Search() {
+const prisma = new PrismaClient();
+
+const fetchRestaurantsByCity = (city: string | undefined) => {
+  const select = {
+    id: true,
+    name: true,
+    main_image: true,
+    price: true,
+    cuisine: true,
+    location: true,
+    slug: true,
+  };
+  if (!city) {
+    return prisma.restaurant.findMany({ select });
+  }
+
+  return prisma.restaurant.findMany({
+    where: {
+      location: {
+        name: {
+          equals: city.toLowerCase(),
+        },
+      },
+    },
+    select,
+  });
+};
+
+async function Search({ searchParams }: { searchParams: { city: string } }) {
+  const restaurants = await fetchRestaurantsByCity(searchParams.city);
+
   return (
     <>
       <Header />
       <div className="m-auto flex w-2/3 items-start justify-between py-4">
         <SearchSideBar />
         <div className="w-5/6">
-          <RestaurantCard />
+          {restaurants.length === 0 && (
+            <div className="mt-10 text-center text-3xl font-bold">
+              No restaurants found
+            </div>
+          )}
+          {restaurants.length > 0 &&
+            restaurants.map((restaurant) => (
+              <RestaurantCard key={restaurant.id} restaurant={restaurant} />
+            ))}
         </div>
       </div>
     </>
