e6a3118546099c8a4e15ff3eab3992b3340ec717
Avishka Umesh <umesh.avishka@gmail.com>
2023-10-10 22:53:35 +0530

    added filtering logic to sidebar

--- app/search/components/SearchSideBar.tsx
+++ app/search/components/SearchSideBar.tsx
@@ -1,33 +1,87 @@
-function SearchSideBar() {
+import { Cuisine, Location, PRICE } from '@prisma/client';
+import Link from 'next/link';
+import { SearchParams } from '../page';
+
+function SearchSideBar({
+  locations,
+  cuisines,
+  searchParams,
+}: {
+  locations: Location[];
+  cuisines: Cuisine[];
+  searchParams: SearchParams;
+}) {
+  const prices = [
+    {
+      price: PRICE.CHEAP,
+      label: '$',
+      className: 'w-full rounded-l border p-2 text-center text-reg font-light',
+    },
+    {
+      price: PRICE.REGULAR,
+      label: '$$',
+      className: 'w-full border p-2 text-center text-reg font-light',
+    },
+    {
+      price: PRICE.EXPENSIVE,
+      label: '$$$',
+      className: 'w-full rounded-r border p-2 text-center text-reg font-light',
+    },
+  ];
+
   return (
-    <div className="w-1/5">
-      <div className="border-b pb-4">
+    <div className="w-1/5 pr-4">
+      <div className="flex flex-col border-b pb-4">
         <h1 className="mb-2">Region</h1>
-        <p className="text-reg font-light">Toronto</p>
-        <p className="text-reg font-light">Ottawa</p>
-        <p className="text-reg font-light">Montreal</p>
-        <p className="text-reg font-light">Hamilton</p>
-        <p className="text-reg font-light">Kingston</p>
-        <p className="text-reg font-light">Niagara</p>
+        {locations.map((location) => (
+          <Link
+            href={{
+              pathname: '/search',
+              query: { ...searchParams, city: location.name },
+            }}
+            key={location.id}
+            className="text-reg font-light capitalize"
+          >
+            {location.name}
+          </Link>
+        ))}
       </div>
-      <div className="mt-3 border-b pb-4">
+      <div className="mt-3 flex flex-col border-b pb-4">
         <h1 className="mb-2">Cuisine</h1>
-        <p className="text-reg font-light">Mexican</p>
-        <p className="text-reg font-light">Italian</p>
-        <p className="text-reg font-light">Chinese</p>
+        {cuisines.map((cuisine) => (
+          <Link
+            href={{
+              pathname: '/search',
+              query: {
+                ...searchParams,
+                cuisine: cuisine.name,
+              },
+            }}
+            key={cuisine.id}
+            className="text-reg font-light capitalize"
+          >
+            {cuisine.name}
+          </Link>
+        ))}
       </div>
       <div className="mt-3 pb-4">
         <h1 className="mb-2">Price</h1>
         <div className="flex">
-          <button className="w-full rounded-l border p-2 text-reg font-light">
-            $
-          </button>
-          <button className="w-full border-b border-r border-t p-2 text-reg font-light">
-            $$
-          </button>
-          <button className="w-full rounded-r border-b border-r border-t p-2 text-reg font-light">
-            $$$
-          </button>
+          {prices.map(({ price, label, className }) => (
+            <Link
+              key={price}
+              href={{
+                pathname: '/search',
+                query: {
+                  ...searchParams,
+                  price: price,
+                },
+              }}
+              className={className}
+            >
+              {label}
+            </Link>
+          ))}
         </div>
       </div>
     </div>
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,8 +1,14 @@
-import { PrismaClient } from '@prisma/client';
+import { PRICE, PrismaClient } from '@prisma/client';
 import { Metadata } from 'next';
 import Header from './components/Header';
 import RestaurantCard from './components/RestaurantCard';
 import SearchSideBar from './components/SearchSideBar';
+
+export interface SearchParams {
+  city?: string;
+  cuisine?: string;
+  price?: PRICE;
+}
 
 export const metadata: Metadata = {
   title: 'Search Restaurants OpenTable',
@@ -11,7 +17,33 @@ export const metadata: Metadata = {
 
 const prisma = new PrismaClient();
 
-const fetchRestaurantsByCity = (city: string | undefined) => {
+const fetchRestaurantsByCity = (searchParams: SearchParams) => {
+  const where: {
+    location?: { name: { equals: string } };
+    cuisine?: { name: { equals: string } };
+    price?: { equals: PRICE };
+  } = {};
+
+  if (searchParams.city) {
+    where.location = {
+      name: {
+        equals: searchParams.city.toLowerCase(),
+      },
+    };
+  }
+  if (searchParams.cuisine) {
+    where.cuisine = {
+      name: {
+        equals: searchParams.cuisine.toLowerCase(),
+      },
+    };
+  }
+  if (searchParams.price) {
+    where.price = {
+      equals: searchParams.price,
+    };
+  }
+
   const select = {
     id: true,
     name: true,
@@ -21,30 +53,35 @@ const fetchRestaurantsByCity = (city: string | undefined) => {
     location: true,
     slug: true,
   };
-  if (!city) {
-    return prisma.restaurant.findMany({ select });
-  }
 
   return prisma.restaurant.findMany({
-    where: {
-      location: {
-        name: {
-          equals: city.toLowerCase(),
-        },
-      },
-    },
+    where,
     select,
   });
 };
 
-async function Search({ searchParams }: { searchParams: { city: string } }) {
-  const restaurants = await fetchRestaurantsByCity(searchParams.city);
+const fetchLocations = async () => {
+  return await prisma.location.findMany();
+};
+
+const fetchCuisines = async () => {
+  return await prisma.cuisine.findMany();
+};
+
+async function Search({ searchParams }: { searchParams: SearchParams }) {
+  const restaurants = await fetchRestaurantsByCity(searchParams);
+  const locations = await fetchLocations();
+  const cuisines = await fetchCuisines();
 
   return (
     <>
       <Header />
       <div className="m-auto flex w-2/3 items-start justify-between py-4">
-        <SearchSideBar />
+        <SearchSideBar
+          locations={locations}
+          cuisines={cuisines}
+          searchParams={searchParams}
+        />
         <div className="w-5/6">
           {restaurants.length === 0 && (
             <div className="mt-10 text-center text-3xl font-bold">
